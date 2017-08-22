dd08528568d260d6441ae03a0bd677deea738735
Bobby <ho.chan@colorado.edu>
2017-08-22 12:47:33 -0600

    Update UltrasonicSerialPassthrough.ino

--- Arduino/UltrasonicSerialPassthrough/UltrasonicSerialPassthrough.ino
+++ Arduino/UltrasonicSerialPassthrough/UltrasonicSerialPassthrough.ino
@@ -4,7 +4,7 @@
  * Date: August 22, 2017
  * 
  * Description: Set up a software serial port to pass data between an ultrasonic
- * range finder and the serial monitor.
+ * range finder and the serial monitor. The data is inverted.
  *
  * Hardware Hookup:
  *
